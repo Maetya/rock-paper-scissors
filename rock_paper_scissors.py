dff0077b1833dc88cb68b7e438b58101d3f6be66
Matthew Vuu <matthewvuu@gmail.com>
2021-12-31 00:01:08 +1100

    Modified code to return the 'computer's choice' for easier code accessibility from rock_paper_scissors_gui

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,64 +14,73 @@ def printHeader():
     print("Enter your move: (R)ock (P)aper (S)cissors or (Q)uit")
 
 
-def showdown():
-    while True:
-        printHeader()
-        userChoice = input().lower()
-        computerChoice = availableChoices[random.randint(0, 2)]
+def showdown(userChoice):
+    global winCount
+    global lossCount
+    global tieCount
+    global computerChoice
+# while True:
+    printHeader()
+    # userChoice = input().lower()
+    computerChoice = availableChoices[random.randint(0, 2)]
 
-        if userChoice == "r":
-            print("ROCK versus...")
-            if computerChoice == "s":
-                winCount = winCount + 1
-                print("SCISSORS")
-                print("You win!")
+    if userChoice == "r":
+        print("ROCK versus...")
+        if computerChoice == "s":
+            winCount = winCount + 1
+            print("SCISSORS")
+            print("You win!")
 
-            if computerChoice == "p":
-                lossCount = lossCount + 1
-                print("PAPER")
-                print("You lose!")
+        if computerChoice == "p":
+            lossCount = lossCount + 1
+            print("PAPER")
+            print("You lose!")
 
-            if computerChoice == "r":
-                tieCount = tieCount + 1
-                print("ROCK")
-                print("It's a tie!")
+        if computerChoice == "r":
+            tieCount = tieCount + 1
+            print("ROCK")
+            print("It's a tie!")
 
 
-        elif userChoice == "p":
-            print("PAPER versus...")
-            if computerChoice == "s":
-                lossCount = lossCount + 1
-                print("SCISSORS")
-                print("You lose!")
+    elif userChoice == "p":
+        print("PAPER versus...")
+        if computerChoice == "s":
+            lossCount = lossCount + 1
+            print("SCISSORS")
+            print("You lose!")
 
-            if computerChoice == "r":
-                winCount = winCount + 1
-                print("ROCK")
-                print("You win!")
+        if computerChoice == "r":
+            winCount = winCount + 1
+            print("ROCK")
+            print("You win!")
 
-            if computerChoice == "p":
-                tieCount = tieCount + 1
-                print("PAPER")
-                print("It's a tie!")
+        if computerChoice == "p":
+            tieCount = tieCount + 1
+            print("PAPER")
+            print("It's a tie!")
 
-        elif userChoice == "s":
-            if computerChoice == "r":
-                lossCount = lossCount + 1
-                print("ROCK")
-                print("You lose!")
+    elif userChoice == "s":
+        if computerChoice == "r":
+            lossCount = lossCount + 1
+            print("ROCK")
+            print("You lose!")
 
-            if computerChoice == "p":
-                winCount = winCount + 1
-                print("PAPER")
-                print("You win!")
+        if computerChoice == "p":
+            winCount = winCount + 1
+            print("PAPER")
+            print("You win!")
 
-            if computerChoice == "s":
-                tieCount = tieCount + 1
-                print("s")
-                print("It's a tie!")
+        if computerChoice == "s":
+            tieCount = tieCount + 1
+            print("s")
+            print("It's a tie!")
 
-        elif userChoice == "q":
-            sys.exit()
+    elif userChoice == "q":
+        sys.exit()
 
-        print(" ")
+
+    print(" ")
+
+    return computerChoice
+
+
