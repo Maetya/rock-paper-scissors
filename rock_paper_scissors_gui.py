70e6ad7cfaed8402e36a651e2b1bf99dc48dbb81
Matthew Vuu <matthewvuu@gmail.com>
2022-01-01 01:15:19 +1100

    GUI now correctly displays details such as the score and a message of the outcome of the game.

--- rock_paper_scissors_gui.py
+++ rock_paper_scissors_gui.py
@@ -22,24 +22,39 @@ def displayPressedButton(userChoice):
     elif computerChoice == "s":
         Label(window, image=img_scissors, bg="white").grid(row=5, column=2, pady=5, sticky=E)
 
-    verseTextBox = Label(text="VS")
+
+
+
+    resultTextBox = Label(text=returnResult(), bg='#ffffff')
+    resultTextBox.grid(row=4, column=1)
+
+    spacer2 = Label(text="", bg='#ffffff')
+    spacer2.grid(row=5, column=0, pady=100)
+
+
+    verseTextBox = Label(text="VS" , bg='#ffffff')
     verseTextBox.grid(row=5, column=1)
 
-window = Tk()
-window.geometry("388x500")
-window.title("Rock Paper and Scissors")
+    scoreTextBox = Label(text=returnScore(),bg='#ffffff')
+    scoreTextBox.grid(row=7, column=1)
 
+
+
+
+
+window = Tk()
+window.geometry("388x508")
+window.title("Rock Paper and Scissors")
+window['background']= '#ffffff'
 img_rock = PhotoImage(file="images/rock.png")
 img_scissors = PhotoImage(file="images/scissors.png")
 img_paper = PhotoImage(file="images/paper.png")
 
 
-chooseOneTextBox = Label(text="Select an icon")
-chooseOneTextBox.grid(row=1, column=1)
 
-spacer1 = Label(window, text="")
-spacer1.grid(row=3, column=0, pady=40)
 
+
+#TODO: Change border colour to white
 rockButton = Button(window, image=img_rock,command= lambda: displayPressedButton("r"), borderwidth=0)
 rockButton.grid(row=2, column=0, sticky=E)
 
@@ -49,10 +64,17 @@ scissorsButton.grid(row=2, column=1)
 paperButton = Button(window, image=img_paper, command= lambda: displayPressedButton("p"), borderwidth=0)
 paperButton.grid(row=2, column=2)
 
-youTextBox = Label(text = "You:")
+chooseOneTextBox = Label(text="Select an icon", fg="black", bg='#ffffff')
+chooseOneTextBox.grid(row=1, column=1)
+
+
+spacer1 = Label(text="", bg='#ffffff')
+spacer1.grid(row=3, column=0, pady=20)
+
+youTextBox = Label(text = "You", bg='#ffffff')
 youTextBox.grid(row=4, column=0)
 
-computerTextBox = Label(text = "Computer: " )
+computerTextBox = Label(text = "Computer", bg='#ffffff' )
 computerTextBox.grid(row=4, column=2)
 
 
